f79f70bcdea31ba8f8186074a98063ae06e5cd36
Guilherme Rocha <luzguilherme.dev@gmail.com>
2025-01-08 12:07:17 -0300

    Criando função de multiplicação e saudação

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+// Multiplicando 2 numeros;
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+;
+// Saudação
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+;
+console.log(multiplicacao(4, 2));
+console.log(saudacao("Guilherme"));
---
+++ script.ts
@@ -0,0 +1,12 @@
+// Multiplicando 2 numeros;
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+};
+
+// Saudação
+function saudacao(nome: string): string {
+    return `Olá ${nome}`
+};
+
+console.log(multiplicacao(4, 2));
+console.log(saudacao("Guilherme"));
